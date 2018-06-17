703c55a571aad5483e6414842cd58092fa920426
Lucas <lucassantosamaral@hotmail.com>
2018-06-16 21:38:25 -0300

    Adding products size list in json

--- build/js/touch.js
+++ build/js/touch.js
@@ -1,4 +1,18 @@
-// Ready to begin!
+$(document).ready(function(){
+
+    $.getJSON("src/js/products-sizes.json", function(data){
+        $.each(data, function(key, value){
+
+            var total = value.length -1;
+            
+            for(i = 0; i<=total; i++) {
+                $('#productSize').append('<option value="'+value[i].val+'">'+value[i].size+'</option>');
+            }
+            
+        });
+    });
+
+});
 
 var otherProducts = new Vue({
     el: "#otherProducts",
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
-// Ready to begin!
+$(document).ready(function(){
+
+    $.getJSON("src/js/products-sizes.json", function(data){
+        $.each(data, function(key, value){
+
+            var total = value.length -1;
+            
+            for(i = 0; i<=total; i++) {
+                $('#productSize').append('<option value="'+value[i].val+'">'+value[i].size+'</option>');
+            }
+            
+        });
+    });
+
+});
