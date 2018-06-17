ecb2cdc9d51a0b4ef281267a5d2b1cf6a7a2b12d
Lucas <lucassantosamaral@hotmail.com>
2018-06-17 00:43:05 -0300

    requirement-2 finished

--- build/js/touch.js
+++ build/js/touch.js
@@ -12,6 +12,18 @@ $(document).ready(function(){
         });
     });
 
+    $('.product__thumb').click(function(){
+
+        var index = ($(this).index()) + 1;
+
+        $('.product__thumb').removeClass('current-picture');
+
+        $(this).addClass('current-picture');
+
+        $('.product__image img').attr('src','src/assets/image-pdp-desktop'+index+'.jpg');
+
+    });
+
 });
 
 var otherProducts = new Vue({
